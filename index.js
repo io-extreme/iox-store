4d5654f44bca2bbe549e154aec49e1a0d2e0ed7c
GleisonMV <gleisonmorgado@hotmail.com>
2019-07-14 09:31:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+/**
+ * iox-store
+ *
+ * Copyright(c) 2019 Gleison M. Vasconcelos
+ * MIT Licensed
+ */
+
 const store = {
   $: {},
-  set(name, value){
-    store.$[name] = value 
+  set (name, value) {
+    store.$[name] = value
     return store
   },
-  get(name){
+  get (name) {
     return store.$[name]
   },
-  delete(name){
+  delete (name) {
     delete store.$[name]
     return store
   }
